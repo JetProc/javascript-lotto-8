b5f0bfedad39f1aeb6df205a3947f0c2e673c4f1
JetProc <soonjae8297@gmail.com>
2025-11-03 23:19:47 +0900

    force re-track all local files after reset

--- src/utils/Utils.js
+++
@@ -1,9 +0,0 @@
-export function splitBySeparator(string, separator) {
-  return string.trim().split(separator);
-}
-
-export function formatNumber(number) {
-  return number.toLocaleString('ko-KR', {
-    maximumFractionDigits: 1,
-  });
-}
