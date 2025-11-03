98fb49570251447cc779b3f9e07adc9a0be4b6c3
JetProc <soonjae8297@gmail.com>
2025-11-03 18:35:25 +0900

    feat: add utils

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,3 +1,9 @@
 export function splitBySeparator(string, separator) {
   return string.trim().split(separator);
 }
+
+export function formatNumber(number) {
+  return number.toLocaleString('ko-KR', {
+    maximumFractionDigits: 1,
+  });
+}
