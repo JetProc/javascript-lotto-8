597bbc841001c2e32b221d532b86e669b0aa152c
JetProc <soonjae8297@gmail.com>
2025-11-03 18:50:02 +0900

    feat: implement printResult logic

--- src/views/OuputView.js
+++ src/views/OuputView.js
@@ -1,6 +1,7 @@
 import { Console } from '@woowacourse/mission-utils';
 import { OUTPUT_MESSAGE } from '../constants/messages.js';
 import { LOTTO_NUMBER_SEPARATOR, LOTTO_RANK } from '../constants/config.js';
+import { formatNumber } from '../utils/Utils.js';
 const OutputView = {
   printPurchasedLottos(lottoNumberArrays) {
     Console.print(OUTPUT_MESSAGE.PURCHASE_RESULT(lottoNumberArrays.length));
@@ -10,27 +11,27 @@ const OutputView = {
     });
   },
 
-  // 3개 일치 (5,000원) - 1개
-  // 4개 일치 (50,000원) - 0개
-  // 5개 일치 (1,500,000원) - 0개
-  // 5개 일치, 보너스 볼 일치 (30,000,000원) - 0개
-  // 6개 일치 (2,000,000,000원) - 0개
-
-  printResult(winningCnt, purchaseMoney, prizeMoney) {
+  printResult(resultData, profitRate) {
     Console.print(OUTPUT_MESSAGE.WINNING_STATISTICS_HEADER);
 
-    LOTTO_RANK.toReversed().forEach((idx, rank) => {
-      const { matchCount, money, requireBonus } = rank;
-      Console.print(
-        `${OUTPUT_MESSAGE.WINNING_MATCH_COUNT_INFO(matchCount)} ${OUTPUT_MESSAGE.WINNING_PRIZE_MONEY(money)} - ${
-          winningCnt[idx]
-        }개`
-      );
-    });
+    this.printWinningInfo(resultData);
+
+    const formattedProfitRate = Number(formatNumber(profitRate)).toFixed(1);
+    Console.print(OUTPUT_MESSAGE.PROFIT_RATE(formattedProfitRate));
   },
 
-  printError(meesage) {
-    Console.print(meesage);
+  printWinningInfo(resultData) {
+    resultData.forEach((rank) => {
+      const { matchCount, money, requireBonus, count } = rank;
+
+      let matchMessage = OUTPUT_MESSAGE.WINNING_MATCH_COUNT_INFO(matchCount);
+      if (requireBonus) matchMessage += ', 보너스 볼 일치';
+
+      const formattedMoney = formatNumber(money);
+      const prizeMessage = OUTPUT_MESSAGE.WINNING_PRIZE_MONEY(formattedMoney);
+
+      Console.print(`${matchMessage} ${prizeMessage} - ${count}개`);
+    });
   },
 };
 
